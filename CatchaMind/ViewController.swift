a8885ab114a0deb203b7c2fd07285befe94f98e4
hyein park <hyeinpark@hyeins-macbook.local>
2018-07-09 09:58:01 +0900

    2018.07.09 setLayout hyein Park

--- CatchaMind/ViewController.swift
+++ CatchaMind/ViewController.swift
@@ -9,10 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let firstView = UIView();
+    let secondView = UIView();
+    let thirdView = UIView();
+    let fourthView = UIView();
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.addSubview(firstView)
+        view.addSubview(secondView)
+        view.addSubview(thirdView)
+        view.addSubview(fourthView)
+        
+        setLayout();
+    }
+    
+    func setLayout() -> Void {
+        firstView.translatesAutoresizingMaskIntoConstraints = false
+        firstView.backgroundColor = UIColor.red
+        firstView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        firstView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        secondView.translatesAutoresizingMaskIntoConstraints = false
+        secondView.backgroundColor = UIColor.blue
+        secondView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        secondView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        secondView.topAnchor.constraint(equalTo: firstView.bottomAnchor).isActive = true
+        
+        thirdView.translatesAutoresizingMaskIntoConstraints = false
+        thirdView.backgroundColor = UIColor.purple
+        thirdView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        thirdView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        thirdView.leftAnchor.constraint(equalTo: firstView.rightAnchor).isActive = true
+        
+        fourthView.translatesAutoresizingMaskIntoConstraints = false
+        fourthView.backgroundColor = UIColor.green
+        fourthView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        fourthView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        fourthView.topAnchor.constraint(equalTo: thirdView.bottomAnchor).isActive = true
+        fourthView.leftAnchor.constraint(equalTo: secondView.rightAnchor).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
